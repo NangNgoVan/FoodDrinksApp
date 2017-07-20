3a0c30ce934640faf83196c6a577e1a64314904a
Nang Ngo <nangnv2641@gmail.com>
2017-07-20 10:16:29 +0700

    login-basic

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170720033128) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
